631dd969d35e43b09a7e7aa90cbc80be838e8986
Enekinxu <enekinxu@gmail.com>
2026-01-28 20:03:40 +0100

    add bot code

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
 client.on("ready", () => {
-  console.log(`Bot encendido como ${client.user.tag}`);
+    console.log(`Bot encendido como ${client.user.tag}`);
 });
 
 client.on("messageCreate", (message) => {
-  if (message.content === "!ping") {
-    message.reply("Pong!");
-  }
+    if (message.content === "ping") {
+        message.reply("Pong!");
+    }
 });
 
 client.login(process.env.TOKEN);
