1119056f1e41038440281f0ba4b3900f5e541c2b
Enekinxu <enekinxu@gmail.com>
2026-01-30 14:31:16 +0100

    a

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers, // ← NECESARIO para detectar nuevos miembros
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent
     ]
 });
 
+// ID del canal donde quieres enviar la bienvenida
+const WELCOME_CHANNEL_ID = "ID_DEL_CANAL";
+
 client.on("ready", () => {
     console.log(`Bot encendido como ${client.user.tag}`);
 });
 
+// Evento de bienvenida
+client.on("guildMemberAdd", (member) => {
+    const channel = member.guild.channels.cache.get(WELCOME_CHANNEL_ID);
+    if (!channel) return;
+
+    channel.send(`🎉 ¡Bienvenido/a **${member.user.username}** a **${member.guild.name}**!`);
+});
+
+// Comando !hola
 client.on("messageCreate", (message) => {
     if (message.content === "!hola") {
         message.reply("¡Hola, bienvenido a BloqueMágico!");
