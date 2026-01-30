db01b4f15aad0efb97b680f86d79eb414bd6e7ba
Enekinxu <enekinxu@gmail.com>
2026-01-30 15:22:35 +0100

    a

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on("guildMemberAdd", (member) => {
     const channel = member.guild.channels.cache.get(WELCOME_CHANNEL_ID);
     if (!channel) return;
 
-    channel.send(`Bienvenid@ {member.user.toString()} a **{member.guild.name}**! Eres el {member.guild.memberCount}º miembro!`);
+    channel.send(`¡Bienvenido/a **@${member.user.username}** a **${member.guild.name}**!`);
 });
 
 // Comando !hola
